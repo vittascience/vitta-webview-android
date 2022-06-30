3665cf6e7c30433878a3d335e00369e55e52a6cd
Maxence Lavenu <maxence.lavenu@outlook.fr>
2022-06-30 12:12:46 +0200

    Initial commit

---
+++ app/src/main/java/com/maxencelav/vittasciencetest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.maxencelav.vittasciencetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = findViewById(R.id.vittaWebView);
+        myWebView.setWebChromeClient(new WebChromeClient());
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setDomStorageEnabled(true);
+
+
+
+        myWebView.loadUrl("https://vittascience.com/microbit?embed=1");
+    }
+}
